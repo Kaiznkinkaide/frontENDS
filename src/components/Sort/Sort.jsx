d2b88a979a740b10de3ad3d57d2260364c2b866b
JohannaHaer <johanna.haerdter@gmail.com>
2024-02-13 14:12:09 +0100

    sportsList aktualisiert

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -7,24 +7,49 @@ const Sort = () => {
     useContext(mainContext);
 
   return (
-    <div className="sortDiv">
-      <select className="select-wrap" onChange={(e) => setSportFilter(e.target.value)}>
-        <option className="optionSort" value="">All Sports</option>
-        {sports.map((sport, index) => (
-          <option key={index} value={sport}>
-            {sport}
-          </option>
-        ))}
-      </select>
-      <select onChange={(e) => setCountryFilter(e.target.value)}>
-        <option value="">All Countries</option>
-        {countries.map((country, index) => (
-          <option key={index} value={country}>
-            {country}
-          </option>
-        ))}
-      </select>
-    </div>
+    <>
+      {/* <div>
+        {sports.map((sport, index) => {
+          return(
+            <div key={index}>
+              <p>{sport}</p>
+              <button>X</button>
+            </div>
+          )
+        })}
+        {countries.map((sport, index) => {
+          return(
+            <div key={index}>
+              <p>{sport}</p>
+              <button>X</button>
+            </div>
+          )
+        })}
+      </div> */}
+      
+      <div className="sortDiv">
+        <select className="select-wrap" onChange={(e) => setSportFilter(e.target.value)}>
+          <option className="optionSort" value="">All Sports</option>
+          {sports.map((sport, index) => {
+            return(
+              <option key={index} value={sport}>
+                {sport}
+              </option>
+            )
+          })}
+        </select>
+        <select onChange={(e) => setCountryFilter(e.target.value)}>
+          <option value="">All Countries</option>
+          {countries.map((country, index) => {
+            return(
+              <option key={index} value={country}>
+                {country}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+    </>
   );
 };
 
